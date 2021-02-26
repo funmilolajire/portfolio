cde25ce95ffa628981dbf2cffd884f05d039cf5e
Funmilola O <funmilolajire@gmail.com>
2021-02-26 03:07:15 +0100

    set up analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,15 @@
 import './App.scss';
+import GA from '../../utils/GoogleAnalytics';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">Hello World</div>
+    <Router>
+      { GA.init() && <GA.RouteTracker />}
+      <Switch>
+        <Route exact path="/" render={() => <div className="App">Hello World</div>} />
+      </Switch>
+    </Router>
   )
 }
 
