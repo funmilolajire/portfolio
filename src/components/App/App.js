f402da6cf0a90915be7fff452aca37892c8a6a81
Funmilola O <funmilolajire@gmail.com>
2021-02-26 03:45:10 +0100

    set up analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,33 @@
 import './App.scss';
-import GA from '../../utils/GoogleAnalytics';
+import ReactGA from 'react-ga';
+// import GA from '../../utils/GoogleAnalytics';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const TRACKING_ID = "UA-190642802-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID, {
+  debug: true,
+  titleCase: false,
+  gaOptions: {
+    // userId: 123,
+    siteSpeedSampleRate: 100
+  }
+});
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+ReactGA.event({
+  category: 'User',
+  action: 'Created an Account'
+});
+
+ReactGA.exception({
+  description: 'An error ocurred',
+  fatal: true
+});
 
 function App() {
   return (
     <Router>
-      { GA.init() && <GA.RouteTracker />}
+      {/* { GA.init() && <GA.RouteTracker />} */}
       <Switch>
         <Route exact path="/" render={() => <div className="App">Hello World</div>} />
       </Switch>
