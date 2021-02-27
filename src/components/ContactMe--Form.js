1f67fe6b9ea962855a260cacb096b7f769953b40
Funmilola O <funmilolajire@gmail.com>
2021-02-28 00:37:04 +0100

    add redirects for netlify

--- src/components/ContactMe--Form.js
+++ src/components/ContactMe--Form.js
@@ -30,15 +30,15 @@ export const ContactForm = () => {
             <form name="portfolio-contact-form" onSubmit={handleSubmit} method="post" className="w-7/12 text-sm" encType="text/plain">
                 <div className="pb-6 name">
                     <label htmlFor="name" className="block pb-2 font-bold opacity-80">Name</label>
-                    <input ref={inputRef} value={name} onChange={handleChange} autoComplete="off" className="block w-full py-2 pl-4 bg-gray-300 rounded-md focus:outline-none focus:ring focus:border-gray-800 placeholder-opacity-40 bg-opacity-40" type="text" name="name" id="name" placeholder="Enter name here..." />
+                    <input ref={inputRef} value={name} onChange={handleChange} autoComplete="off" className="block w-full py-2 pl-4 bg-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500 placeholder-opacity-40 bg-opacity-40" type="text" name="name" id="name" placeholder="Enter name here..." />
                 </div>
                 <div className="pb-6 email">
                     <label htmlFor="email" className="block pb-2 font-bold opacity-80">Email Address</label>
-                    <input value={email} onChange={handleChange} className="block w-full py-2 pl-4 bg-gray-300 rounded-md focus:outline-none focus:ring focus:border-gray-800 placeholder-opacity-40 bg-opacity-40" type="email" name="email" id="email" placeholder="Enter email here..." />
+                    <input value={email} onChange={handleChange} className="block w-full py-2 pl-4 bg-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-green-500 placeholder-opacity-40 bg-opacity-40" type="email" name="email" id="email" placeholder="Enter email here..." />
                 </div>
                 <div className="pb-6 message">
                     <label htmlFor="message" className="block pb-2 font-bold opacity-80">Message</label>
-                    <textarea value={message} onChange={handleChange} className="block w-full py-2 pl-4 bg-gray-300 rounded-md resize-none placeholder-opacity-40 focus:outline-none focus:ring focus:border-gray-800 bg-opacity-40" name="message" id="message" cols="30" rows="10" placeholder="How can I help?"></textarea>
+                    <textarea value={message} onChange={handleChange} className="block w-full py-2 pl-4 bg-gray-300 rounded-md resize-none placeholder-opacity-40 focus:outline-none focus:ring-1 focus:ring-green-500 bg-opacity-40" name="message" id="message" cols="30" rows="10" placeholder="How can I help?"></textarea>
                 </div>
                 <button type="submit" style={{ backgroundColor: "#203A4C", letterSpacing: "2px" }} className="h-12 text-xs font-bold text-white uppercase rounded-md w-52 focus:outline-none focus:ring focus:border-gray-800">Send Message</button>
             </form>
