924c08bb551d92a2d8c0789452dbebe9b90712f8
Funmilola O <funmilolajire@gmail.com>
2021-04-12 20:24:29 +0100

    style changes

--- src/components/Home--About.js
+++ src/components/Home--About.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const About = () => (
     <div className="Home--About" data-about-me id="about-me">
-        <picture>
+        {/* <picture>
             <source media="(min-width: 880px)"
                 srcset="/images/homepage/desktop/image-homepage-profile.jpg,
                 /images/homepage/desktop/image-homepage-profile@2x.jpg 2x" />
@@ -11,14 +11,14 @@ export const About = () => (
                 /images/homepage/tablet/image-homepage-profile@2x.jpg 2x" />
             <img loading="lazy" alt="home-profile" srcset="/images/homepage/mobile/image-homepage-profile.jpg,
                 /images/homepage/mobile/image-homepage-profile@2x.jpg 2x"/>
-        </picture>
+        </picture> */}
         <article>
             <h2>About Me</h2>
-            <p>I’m a junior front-end developer looking for freelance opportunities or a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices/frameworks and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I have experience building small and medium websites, web apps, using animation libraries such as gsap and framer motion. I also have full-stack developer experience working with nodejs, expressjs, MySql, MongoDB,{/* Postgresql, firebase*/} and some other tools and frameworks. I’m based in Lagos, Nigeria, but I’m happy working remotely. I’d love you to check out my work.</p>
+            <p>I’m a junior front-end developer looking for freelance opportunities or a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices/frameworks and writing clean JavaScript/TypeScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I have experience building small and medium websites, web apps, using animation libraries such as gsap and framer motion. I also have full-stack developer experience working with nodejs, expressjs, nestjs, MySql, MongoDB, PostgreSql{/* , firebase*/} and some other tools and frameworks. I’m based in Lagos, Nigeria, but I’m happy working remotely. I’d love you to check out my work.</p>
             <Link to="/projects">
                 <button>
                     Go To Portfolio
-                    </button>
+                </button>
             </Link>
         </article>
     </div>
